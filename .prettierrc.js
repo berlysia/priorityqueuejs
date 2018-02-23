981b7e6ee09c7bd0b738ca1799e41872f8876cea
berlysia <berlysia@gmail.com>
2018-02-23 22:16:10 +0900

    chore(npm): update environment

---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "parser": "babylon",
+  "printWidth": 80,
+  "trailingComma": "es5"
+};
---
+++ test/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "globals": {
+    "describe": false,
+    "it": false,
+    "test": false,
+    "beforeEach": false,
+    "afterEach": false,
+    "beforeAll": false,
+    "afterAll": false,
+    "expect": false,
+    "jest": false,
+  }
+};
