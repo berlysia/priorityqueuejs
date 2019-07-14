60c3997f889250414ed8015041416a6cd2e800c4
berlysia <berlysia@gmail.com>
2019-07-15 02:43:41 +0900

    perf: add filter

--- src/_internal/performance/index.js
+++ src/_internal/performance/index.js
@@ -1,6 +1,19 @@
+import * as builder from "yargs";
 import * as statistics from "simple-statistics";
 import { BinaryHeap, PairingHeap, SkewHeap } from "../../";
 import operations from "./operations";
+
+const { testCase, algorithm } = builder
+  .option("t", {
+    alias: "testCase",
+    describe: "filter by test case name",
+    type: "string",
+  })
+  .option("a", {
+    alias: "algorithm",
+    describe: "filter by algorithm name",
+    type: "string",
+  }).argv;
 
 const HeapCtors = [BinaryHeap, PairingHeap, SkewHeap];
 const tests = [...operations];
@@ -16,9 +29,11 @@ const stats = values => ({
 
 const sizes = [100, 1000, 10000];
 
-function runBenchmarks() {
+function runBenchmarks(ctorRegex, nameRegex) {
   for (const test of tests) {
+    if (nameRegex && !nameRegex.test(test.name)) continue;
     for (const Ctor of HeapCtors) {
+      if (ctorRegex && !ctorRegex.test(Ctor.name)) continue;
       for (const size of sizes) {
         const measures = test(Ctor, size);
         if (measures.length) {
@@ -36,4 +51,7 @@ function runBenchmarks() {
   }
 }
 
-runBenchmarks();
+const nameRegex = testCase && new RegExp(testCase);
+const ctorRegex = algorithm && new RegExp(algorithm);
+
+runBenchmarks(ctorRegex, nameRegex);
