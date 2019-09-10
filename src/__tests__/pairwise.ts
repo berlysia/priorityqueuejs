de7e819ae89e62a1ef265e8a92c0ec35190b407f
berlysia <berlysia@gmail.com>
2019-09-11 03:33:08 +0900

    test: merge with different comparators

--- src/__tests__/pairwise.ts
+++ src/__tests__/pairwise.ts
@@ -1,4 +1,4 @@
-import { numericGreaterFirst } from "../comparator";
+import { numericGreaterFirst, dictOrderGreaterFirst } from "../comparator";
 import { BinaryHeap } from "../BinaryHeap";
 import { PairingHeap } from "../PairingHeap";
 import { SkewHeap } from "../SkewHeap";
@@ -8,7 +8,7 @@ const Ctors = [BinaryHeap, PairingHeap, SkewHeap];
 for (const A of Ctors) {
   for (const B of Ctors) {
     describe(`${A.name} vs ${B.name}`, () => {
-      it("mergable", () => {
+      it("mergable if have same comparator", () => {
         // @ts-ignore
         const a = new A({ comparator: numericGreaterFirst });
         a.push(1);
@@ -29,6 +29,29 @@ for (const A of Ctors) {
         expect(a.toArray()).toStrictEqual([1, 2, 3, 4, 5, 6]);
         expect(b.toArray()).toStrictEqual([]);
         expect(a.pop()).toBe(6);
+      });
+
+      it("mergable even if have different comparator", () => {
+        // @ts-ignore
+        const a = new A({ comparator: dictOrderGreaterFirst });
+        a.push(1);
+        a.push(10);
+        a.push(100);
+
+        // @ts-ignore
+        const b = new B({ comparator: numericGreaterFirst });
+        b.push(2);
+        b.push(20);
+        b.push(200);
+
+        expect(a.toArray()).toStrictEqual([1, 10, 100]);
+        expect(b.toArray()).toStrictEqual([2, 20, 200]);
+
+        a.merge(b);
+
+        expect(a.toArray()).toStrictEqual([1, 10, 100, 2, 20, 200]);
+        expect(b.toArray()).toStrictEqual([]);
+        expect(a.pop()).toBe(200);
       });
     });
   }
