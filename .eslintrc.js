5d44b441ab9656b4fd9ea9d31462cb5de69b99ac
berlysia <berlysia@gmail.com>
2020-03-31 05:11:41 +0900

    chore: ignore strict lint rule in test/perf codes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 /* eslint-disable import/no-commonjs */
 module.exports = {
   extends: ["@berlysia/eslint-config/auto"],
+  overrides: [
+    {
+      files: ["src/testing/**/*.ts", "src/performance/**/*.ts"],
+      rules: {
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+      },
+    },
+  ],
 };
